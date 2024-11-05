3a62e171df84794a70bdd885f55ab14603b42a46
KimRPG <120882082+KimRPG@users.noreply.github.com>
2024-11-05 12:08:49 +0900

    git init

---
+++ src/main/java/com/example/git_practice/GitController.java
@@ -0,0 +1,12 @@
+package com.example.git_practice;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GitController {
+    @PostMapping("/git")
+    public String git() {
+        return "git";
+    }
+}
