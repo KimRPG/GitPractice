9708f117290fa9f0065db90da979be3ed2cc0b11
KimRPG <120882082+KimRPG@users.noreply.github.com>
2024-11-05 22:14:49 +0900

    patch

--- src/main/java/com/example/git_practice/GitController.java
+++ src/main/java/com/example/git_practice/GitController.java
@@ -1,5 +1,6 @@
 package com.example.git_practice;
 
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,10 @@ public class GitController {
     @PostMapping("/git")
     public String post() {
         return "git";
+    }
+
+    @PatchMapping("/git")
+    public String patch() {
+        return "patch";
     }
 }
