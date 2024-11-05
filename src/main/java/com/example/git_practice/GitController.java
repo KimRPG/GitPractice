b92ad3c87c8d3573865de1c962cb41dccba3f132
KimRPG <120882082+KimRPG@users.noreply.github.com>
2024-11-05 22:11:08 +0900

    get

--- src/main/java/com/example/git_practice/GitController.java
+++ src/main/java/com/example/git_practice/GitController.java
@@ -1,5 +1,6 @@
 package com.example.git_practice;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,10 @@ public class GitController {
     @PostMapping("/git")
     public String post() {
         return "git";
+    }
+
+    @GetMapping("/git")
+    public String get() {
+        return "get";
     }
 }
